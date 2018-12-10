977e263d116ba2bfe21e2175f96d5d9a4a02b2ee
amel294 <amel294@gmail.com>
2018-12-10 06:17:19 +0530

    GOOD MNG

--- ml/day4/q1.py
+++ ml/day4/q1.py
@@ -6,8 +6,6 @@ X,y_true=make_blobs(n_samples=300,centers=4,cluster_std=0.60)
 kmeans=KMeans(n_clusters=4)
 kmeans.fit(X)
 y_kmeans=kmeans.predict(X)
-plt.scatter(X[:,0],X[:,1],s=50)
-plt.show()
 plt.scatter(X[:,0],X[:,1],s=50,c=y_kmeans,cmap='viridis')
 plt.show()
 
